5e71e221a17aa88f65778429fb4a43d3fc6c10a3
shahar240 <shahar240@gmail.com>
2019-01-10 12:23:56 +0200

    added comments support

---
+++ social-network-server/Social-Common/Models/Dtos/ReturnedCommentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social_Common.Models.Dtos
+{
+    public class ReturnedCommentDto : Comment
+    {
+        User CreatedBy { get; set; }
+        List<User> Referencing { get; set; }
+        string PostId { get; set; } 
+    }
+}
--- social-network-server/SocialDal/Repositories/Neo4j/Neo4jBaseRepository.cs
+++ social-network-server/SocialDal/Repositories/Neo4j/Neo4jBaseRepository.cs
@@ -4,103 +4,105 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
-using System.Text;
 
-public abstract class Neo4jBaseRepository
+namespace SocialDal.Repositories.Neo4j
 {
-    protected IDriver _driver;
-    public Neo4jBaseRepository()
+    public abstract class Neo4jBaseRepository
     {
-        string neo4jUrl = ConfigurationManager.AppSettings["neo4jUrl"];
-        string neo4jUn = ConfigurationManager.AppSettings["neo4jUn"];
-        string neo4jPass = ConfigurationManager.AppSettings["neo4jPass"];
-        _driver = GraphDatabase.Driver(neo4jUrl, AuthTokens.Basic(neo4jUn, neo4jPass));
-    }
-
-    ~Neo4jBaseRepository()
-    {
-        _driver.Dispose();
-    }
-
-    protected IStatementResult Query(string query)
-    {
-        using (ISession session = _driver.Session())
+        protected IDriver _driver;
+        public Neo4jBaseRepository()
         {
-            return session.Run(query);
-
+            string neo4jUrl = ConfigurationManager.AppSettings["neo4jUrl"];
+            string neo4jUn = ConfigurationManager.AppSettings["neo4jUn"];
+            string neo4jPass = ConfigurationManager.AppSettings["neo4jPass"];
+            _driver = GraphDatabase.Driver(neo4jUrl, AuthTokens.Basic(neo4jUn, neo4jPass));
         }
 
-    }
-
-    protected void Create(object obj)
-    {
-        string type = obj.GetType().Name;
-        string json = ObjectToJson(obj);
-
-        string query = $"create(p:{type}{json})";
-        Query(query);
-    }
-
-    protected static List<T> RecordsToList<T>(IEnumerable<IRecord> records) where T : new()
-    {
-        var list = new List<T>();
-        foreach (IRecord record in records)
+        ~Neo4jBaseRepository()
         {
-            var nodeProps = JsonConvert.SerializeObject(record[0].As<INode>().Properties);
-            list.Add(JsonConvert.DeserializeObject<T>(nodeProps));
+            _driver.Dispose();
         }
-            //list.Add(RecordToObject<T>(record));
-        return list;
-    }
 
-    protected static T RecordToObject<T>(IRecord record) where T : new()
-    {
-        var values = record.Values;
-        Type type = typeof(T);
-        T obj = new T();
-        foreach (var value in values)
+        protected IStatementResult Query(string query)
         {
-            var prop = type.GetProperty(value.Key);
-            if (prop != null)
+            using (ISession session = _driver.Session())
             {
-                var castedValue = Convert.ChangeType(value.Value, prop.PropertyType);
-                if (prop.CanWrite)
+                return session.Run(query);
+
+            }
+
+        }
+
+        protected void Create(object obj)
+        {
+            string type = obj.GetType().Name;
+            string json = ObjectToJson(obj);
+
+            string query = $"create(p:{type}{json})";
+            Query(query);
+        }
+
+        protected static List<T> RecordsToList<T>(IEnumerable<IRecord> records) where T : new()
+        {
+            var list = new List<T>();
+            foreach (IRecord record in records)
+            {
+                var nodeProps = JsonConvert.SerializeObject(record[0].As<INode>().Properties);
+                list.Add(JsonConvert.DeserializeObject<T>(nodeProps));
+            }
+            //list.Add(RecordToObject<T>(record));
+            return list;
+        }
+
+        protected static T RecordToObject<T>(IRecord record) where T : new()
+        {
+            var values = record.Values;
+            Type type = typeof(T);
+            T obj = new T();
+            foreach (var value in values)
+            {
+                var prop = type.GetProperty(value.Key);
+                if (prop != null)
                 {
-                    prop.SetValue(obj, castedValue);
+                    var castedValue = Convert.ChangeType(value.Value, prop.PropertyType);
+                    if (prop.CanWrite)
+                    {
+                        prop.SetValue(obj, castedValue);
+                    }
+                }
+                else
+                {
+                    FlatenObject(type, obj, value, prop);
                 }
             }
-            else
+            return obj;
+        }
+
+        protected static void FlatenObject<T>(Type type, T obj, KeyValuePair<string, object> value, System.Reflection.PropertyInfo prop) where T : new()
+        {
+            var srcObj = value.Value;
+            foreach (var srcProp in srcObj.GetType().GetProperties())
             {
-                FlatenObject(type, obj, value, prop);
+                var destProp = type.GetProperty(srcProp.Name);
+                var castedValue = Convert.ChangeType(srcProp.GetValue(srcObj), destProp.PropertyType);
+                if (destProp != null && prop.CanWrite)
+                {
+                    destProp.SetValue(obj, castedValue);
+                }
             }
         }
-        return obj;
-    }
 
-    protected static void FlatenObject<T>(Type type, T obj, KeyValuePair<string, object> value, System.Reflection.PropertyInfo prop) where T : new()
-    {
-        var srcObj = value.Value;
-        foreach (var srcProp in srcObj.GetType().GetProperties())
+        protected static string ObjectToJson(object obj)
         {
-            var destProp = type.GetProperty(srcProp.Name);
-            var castedValue = Convert.ChangeType(srcProp.GetValue(srcObj), destProp.PropertyType);
-            if (destProp != null && prop.CanWrite)
+            var serializer = new JsonSerializer();
+            var stringWriter = new StringWriter();
+            using (var writer = new JsonTextWriter(stringWriter))
             {
-                destProp.SetValue(obj, castedValue);
+                writer.QuoteName = false;
+                serializer.Serialize(writer, obj);
             }
+            return stringWriter.ToString();
         }
-    }
-
-    protected static string ObjectToJson(object obj)
-    {
-        var serializer = new JsonSerializer();
-        var stringWriter = new StringWriter();
-        using (var writer = new JsonTextWriter(stringWriter))
-        {
-            writer.QuoteName = false;
-            serializer.Serialize(writer, obj);
-        }
-        return stringWriter.ToString();
     }
 }
 
