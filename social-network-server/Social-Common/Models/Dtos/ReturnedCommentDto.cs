605ee6e33478edf416d92a94ce59856d753e16e1
shahar240 <shahar240@gmail.com>
2019-01-14 10:41:46 +0200

    added get post comments

--- social-network-server/Social-Common/Models/Dtos/ReturnedCommentDto.cs
+++ social-network-server/Social-Common/Models/Dtos/ReturnedCommentDto.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Social_Common.Models.Dtos
 {
-    public class ReturnedCommentDto : Comment
+    public class ReturnedCommentDto
     {
-        User CreatedBy { get; set; }
-        List<User> Referencing { get; set; }
-        string PostId { get; set; } 
+        [JsonProperty(PropertyName = "commentId")]
+        public string CommentId { get; set; }
+        [JsonProperty(PropertyName = "imgUrl")]
+        public string ImgUrl { get; set; }
+        [JsonProperty(PropertyName = "content")]
+        public string Content { get; set; }
+        [JsonProperty(PropertyName = "time")]
+        public DateTime CreatedOn { get; set; }
+        [JsonProperty(PropertyName = "User")]
+        public User CreatedBy { get; set; }
+        [JsonProperty(PropertyName = "referencing")]
+        public List<User> Referencing { get; set; }
+        [JsonProperty(PropertyName = "postId")]
+        public string PostId { get; set; }
+        [JsonProperty(PropertyName = "isLiked")]
+        public bool IsLiked { get; set; }
+        [JsonProperty(PropertyName = "numberOfLikes")]
+        public int Likes { get; set; }
     }
 }
