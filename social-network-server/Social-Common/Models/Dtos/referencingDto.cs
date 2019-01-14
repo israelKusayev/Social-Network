4d1923440bdebca2122fe01230136f021994e42d
ישראל קוסייב <israel.kusayev@gmail.com>
2019-01-14 11:07:31 +0200

    added post referencing

---
+++ social-network-server/Social-Common/Models/Dtos/referencingDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social_Common.Models.Dtos
+{
+    public class ReferencingDto
+    {
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public int StartIndex { get; set; }
+        public int EndIndex { get; set; }
+    }
+}
