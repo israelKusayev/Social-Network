49b55db0b73a7b783852d637cf90b5c6b477481f
ישראל קוסייב <israel.kusayev@gmail.com>
2019-01-22 09:18:30 +0200

    added server token to header

--- social-network-server/SocialBl/Helpers/ServerComunication.cs
+++ social-network-server/SocialBl/Helpers/ServerComunication.cs
@@ -12,11 +12,13 @@ namespace SocialBl.Helpers
     public class ServerComunication : IServerComunication
     {
         private readonly string _notificationsUrl = ConfigurationManager.AppSettings["NotificationsServiceUrl"];
+        private readonly string _serverToken = ConfigurationManager.AppSettings["ServerToken"];
 
         public void NotifyUser(string action, object obj)
         {
             using (var http = new HttpClient())
             {
+                http.DefaultRequestHeaders.Add("x-auth-token", _serverToken);
                 var response = http.PostAsJsonAsync(_notificationsUrl + action, obj).Result;
                 if (!response.IsSuccessStatusCode)
                 {
